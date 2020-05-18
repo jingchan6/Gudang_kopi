d2be5ad32781f8bf081d6288064966bd390adbe7
Tasya Karinda <50197748+jingchan6@users.noreply.github.com>
2020-05-18 12:55:47 +0700

    Penambahan Fungsi Button

---
+++ src/tubesp/src/tubesp/mainProgram.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesp;
+/**
+ *
+ * @author Tasya
+ */
+public class mainProgram {
+    public static void main(String[] args){
+        Beranda masuk = new Beranda();
+        masuk.setVisible(true);
+    }
+}
